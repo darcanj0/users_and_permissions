94495a7d24607844e0ba177205fa913080f40901
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2023-04-11 18:58:01 -0300

    debug: chmod to all scripts

--- fullSolution.sh
+++ fullSolution.sh
@@ -1,3 +1,32 @@
+#! /bin/bash
+su
+
+echo "Creating folders..."
+
+mkdir publico adm ven sec
+
+echo "Folders successfully created!"
+
+#groups
+echo "Creating groups..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Groups successfully created!"
+
+#group folders
+echo "Adding groups to its folders..."
+
+chown root publico
+chown root:GRP_ADM adm
+chown root:GRP_VEN ven 
+chown root:GRP_SEC sec
+
+echo "Done!"
+
+#users
 echo "Creating users..."
 
 useradd -N -m -c "Carlos" -G GRP_ADM -s /bin/bash -p $(openssl passwd senha123) carlos
@@ -13,3 +42,9 @@ useradd -N -m -c "Amanda" -G GRP_SEC -s /bin/bash -p $(openssl passwd senha123) 
 useradd -N -m -c "Rogério" -G GRP_SEC -s /bin/bash -p $(openssl passwd senha123) rogerio
 
 echo "Users successfully created!"
+
+#permissions
+chmod 777 publico
+chmod 770 adm
+chmod 770 ven
+chmod 770 sec
